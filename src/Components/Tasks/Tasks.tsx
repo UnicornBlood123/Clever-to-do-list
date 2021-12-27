af991fc0ea2338de74bc415ef5f4010f2753322f
UnicornBlood123 <gogulin.vova@mail.ru>
2021-12-27 10:27:11 +0300

    commit_4

--- src/Components/Tasks/Tasks.tsx
+++ src/Components/Tasks/Tasks.tsx
@@ -4,11 +4,10 @@ import Task from "../Task/Task";
 import {useParams} from "react-router-dom";
 
 
-function Tasks({tasks, update}: any) {
+function Tasks({tasks, update, first}: any) {
     const paramsId = useParams().id;
     const [value, setValue] = useState<boolean[]>(tasks.map((task:any)=>task.done));
     const data: any = [];
-    let count = 0;
 
     React.useEffect(() => {
         update(value, paramsId)
@@ -16,7 +15,6 @@ function Tasks({tasks, update}: any) {
 
     tasks?.forEach((task: any, i: number) => {
         if (task.day === paramsId) {
-            count++;
             data.push(
                 <div key={task.id} className="TaskLine">
                     <input type="checkbox" id={i.toString()} checked={value[i]} onChange={e => {
@@ -28,24 +26,11 @@ function Tasks({tasks, update}: any) {
                     {<Task id={task.id} name={task.name}/>}
                 </div>
             )
-        } else if (!paramsId && task.day === '10') {
-            count++;
-            data.push(
-                <div key={task.id} className="TaskLine">
-                    <input type="checkbox" id={i.toString()} checked={value[i]} onChange={e => setValue(
-                        (current: boolean[]) => {
-                            return [...current.slice(0, i), e.target.checked, ...current.slice(i + 1, current.length + 1)]
-                        })
-
-                    }/>
-                    {<Task id={task.id} name={task.name}/>}
-                </div>
-            );
         }
     })
     return (
         <div className="Tasks">
-            <div>{count} Tasks today</div>
+            <div>{data.length} Tasks today</div>
             {data}
         </div>
     );
